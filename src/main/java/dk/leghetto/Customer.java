14a534896e5266995ea80b8796f67dd48ba37234
sebylizz <sebastiankjandersen@gmail.com>
2024-10-29 10:03:59 +0100

    Password hash etc

--- src/main/java/dk/leghetto/Customer.java
+++ src/main/java/dk/leghetto/Customer.java
@@ -1,10 +1,22 @@
 package dk.leghetto;
 
-import jakarta.persistence.*;
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.Username;
+import io.quarkus.security.jpa.UserDefinition;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@UserDefinition
 @Table(name = "users", schema = "dev")
-public class Customer {
+public class Customer extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,43 +28,68 @@ public class Customer {
     private String lastName;
 
     @Column(name = "telephone")
-    private int telephone;
+    private Integer telephone;
+
+    @Username
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "newsletter")
+    private Boolean newsletter;
+
+    @Password
+    @Column(name = "password")
+    private String password;
+
+    @Roles
+    @Column(name = "role")
+    private String role;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.newsletter = false;
+        this.password = BcryptUtil.bcryptHash(password);
+        this.role = "user";
+    }
+
+    public boolean matchPsw(String password) {
+        return BcryptUtil.matches(password, this.password);
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName (String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getTelephone() {
+    public Integer getTelephone() {
         return telephone;
     }
 
-    public void setTelephone (int telephone) {
-        this.telephone = telephone;
+    public String getEmail() {
+        return email;
     }
 
-    @Override
-    public String toString() {
-        return firstName + lastName + telephone;
+    public String getPasswordHash() {
+        return password;
+    }
+
+    public Boolean getNewsletter() {
+        return newsletter;
+    }
+
+    public String getRoles() {
+        return role;
     }
 }
-
--- src/main/java/dk/leghetto/CustomerResource.java
+++ src/main/java/dk/leghetto/CustomerResource.java
@@ -1,10 +1,14 @@
 package dk.leghetto;
 
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 
 @Path("/customers")
 public class CustomerResource {
@@ -17,4 +21,34 @@ public class CustomerResource {
     public List<Customer> listPersons() {
         return customerRepository.listAll();
     }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/addcustomer")
+    public Response addCustomer(
+            @Parameter(description = "First name", required = true) @QueryParam("first_name") String first_name,
+            @Parameter(description = "Last name", required = true) @QueryParam("last_name") String last_name,
+            @Parameter(description = "Email address", required = true) @QueryParam("email") String email,
+            @Parameter(description = "Password", required = true) @QueryParam("password") String password) {
+        customerRepository.add(first_name, last_name, email, password);
+        return Response.ok().build();
+    }
+
+    @GET
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/checklogin")
+    public Response login(
+            @Parameter(description = "Email address", required = true) @QueryParam("email") String email,
+            @Parameter(description = "Password", required = true) @QueryParam("password") String password) {
+        Customer c = customerRepository.findByEmail(email);
+        if (c != null) {
+            if (c.matchPsw(password)) {
+                return Response.ok().build();
+            }
+        }
+        return Response.status(404).build();
+    }
+
 }
