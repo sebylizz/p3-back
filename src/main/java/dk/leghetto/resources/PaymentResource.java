5fa00cc37311d5d6e4166e137ff25b409b1ef30b
LukasTN03 <DL48WD@student.aau.dk>
2024-11-19 15:40:28 +0100

    yu

--- src/main/java/dk/leghetto/resources/PaymentResource.java
+++ src/main/java/dk/leghetto/resources/PaymentResource.java
@@ -1,11 +1,16 @@
 package dk.leghetto.resources;
 
+import java.util.Collections;
+
 import com.stripe.exception.StripeException;
 import dk.leghetto.classes.Order;
 import dk.leghetto.classes.PaymentRequest;
 import dk.leghetto.classes.ProductRepository;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -31,5 +36,16 @@ public class PaymentResource {
         paymentRequest.paymentRequest(test);
 
         return Response.ok().build();
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/carttest")
+    public Response cart(Order order) {
+        
+
+
+        return Response.ok("penis").build();
     }
 }
