cc770f2e68973510663d972fb3108d13b5a7663d
sebylizz <sebastiankjandersen@gmail.com>
2024-11-22 14:00:58 +0100

    Working stuff n thangs

--- src/main/java/dk/leghetto/classes/ProductRepository.java
+++ src/main/java/dk/leghetto/classes/ProductRepository.java
@@ -3,6 +3,7 @@ package dk.leghetto.classes;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import dk.leghetto.classes.ProductDTO.ColorDTO;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -18,11 +19,27 @@ public class ProductRepository implements PanacheRepository<Product> {
                     productDTO.setDescription(product.getDescription());
                     productDTO.setCategoryId(product.getCategory().getId());
                     productDTO.setCollectionId(product.getCollection().getId());
-                    productDTO.setMainImage(product.getColors().getFirst().getMainImage());
-                    productDTO.setColors(product.getColors());
                     productDTO.setPrice(product.getPrice().getPrice());
+                    productDTO.setColors(product.getVariants().stream()
+                            .map(variant -> {
+                                ColorDTO colorDTO = new ColorDTO();
+                                colorDTO.setId(variant.getColor().getId());
+                                colorDTO.setName(variant.getColor().getColor().getName());
+                                colorDTO.setMainImage(variant.getColor().getMainImage());
+                                colorDTO.setImages(variant.getColor().getImages());
+                                colorDTO.setVariants(variant.getColor().getVariants().stream()
+                                        .map(colorVariant -> {
+                                            ColorDTO.VariantDTO variantDTO = new ColorDTO.VariantDTO();
+                                            variantDTO.setId(colorVariant.getId());
+                                            variantDTO.setSize(colorVariant.getSize().getName());
+                                            variantDTO.setQuantity(colorVariant.getQuantity());
+                                            return variantDTO;
+                                        }).collect(Collectors.toList()));
+                                                               
+                                return colorDTO;
+                            }).collect(Collectors.toList()));
+                    productDTO.setMainImage(product.getVariants().get(0).getColor().getMainImage());
                     return productDTO;
-                })
-                .collect(Collectors.toList());
+                }).collect(Collectors.toList());
     }
 }
