316fedcb58ef9bc69705cc6f8fb44c61d1045534
LukasTN03 <DL48WD@student.aau.dk>
2025-01-28 17:53:24 +0100

    Update OrderDetailsResource.java

--- src/main/java/dk/leghetto/resources/OrderDetailsResource.java
+++ src/main/java/dk/leghetto/resources/OrderDetailsResource.java
@@ -14,6 +14,7 @@ import jakarta.ws.rs.core.SecurityContext;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Map;
 
 @Path("/orderDetails")
 @Consumes(MediaType.APPLICATION_JSON)
